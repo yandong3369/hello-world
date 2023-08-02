efd9ac2d312733a9d65319380ac5897718578097
GitHub Action <action@github.com>
2023-08-02 02:54:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/yandong3369/helloworld/MyAction.kt
+++ src/main/kotlin/com/github/yandong3369/helloworld/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yandong3369.helloworld
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
